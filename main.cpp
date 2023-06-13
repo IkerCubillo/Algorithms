efda9109fc252a7a12b24a9bbbed49673c0b9718
IkerCubillo <i.cubillo@opendeusto.es>
2023-06-13 17:45:27 +0200

    Main creado, puntos iniciales temporales

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "Punto.h"
+
+#include <iostream>
+using namespace std;
+
+
+int main(void)
+{
+	Punto p1 = {"p10", 10, 10};
+	Punto p2 = {"p11", 11, 11};
+	Punto p3 = {"p12", 12, 12};
+
+	Punto p4 = {"p20", 20, 20};
+	Punto p5 = {"p21", 21, 21};
+	Punto p6 = {"p22", 22, 22};
+
+	Punto p6 = {"p30", 30, 30};
+	Punto p7 = {"p31", 31, 31};
+	Punto p8 = {"p32", 32, 32};
+
+	return 0;
+}
