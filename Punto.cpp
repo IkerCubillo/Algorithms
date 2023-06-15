1136548afe63ca3fd56f8a8b1a66d68c157fcdfc
IkerCubillo <i.cubillo@opendeusto.es>
2023-06-15 09:53:15 +0200

    Punto operators

--- Punto.cpp
+++ Punto.cpp
@@ -73,56 +73,45 @@ Punto Punto::operator+ (const Punto& p)const
     return resultado;
 }
 
-Punto Punto::operator+ (const Punto& p)const;
-	Punto Punto::operator- (const Punto& p)const;
+Punto Punto::operator- (const Punto& p)const
+{
+    int x = this->getX()-p.getX();
+    int y = this->getY()-p.getY();
+
+    Punto resultado (x, y);
+
+    return resultado;
+}
 	
-    Punto& Punto::operator+= (const Punto& p);
-    Punto& Punto::operator-= (const Punto& p);
+Punto& Punto::operator+= (const Punto& p)
+{   
+    this->setX(this->getX()+p.getX());
+    this->setY(this->getY()+p.getY());
+
+    return *this;
+}
+
+Punto& Punto::operator-= (const Punto& p)
+{   
+    this->setX(this->getX()-p.getX());
+    this->setY(this->getY()-p.getY());
+
+    return *this;
+}
 
 Punto& Punto::operator/ (int divisor)const
 {
-    this->x = this->x/divisor;
-    this->x = this->x/divisor;
+    int x = (this->getX()/divisor);
+    int y = (this->getY()/divisor);
+
+    Punto resultado (x, y);
+    
+    return resultado;
 
 }
 
 Punto& Punto::operator= (const Punto& p)
 {
-    delete[] this->nombre;
-    this->nombre = new char[strlen(p.nombre)+1];
-    strcpy(this->nombre, p.nombre);
-
 	this->x = x;
 	this->y = y;
-}
-
-Punto3D& Punto3D::operator+= (const Punto3D& p)
-{
-    this->setX(this->getX()+p.getX());
-    this->setY(this->getY()+p.getY());
-    this->setZ(this->getZ()+p.getZ());
-
-     return *this;
-}
-
-Punto3D Punto3D::operator* (const Punto3D& punto2)const
-{
-    int x = this->getX()*punto2.getX();
-    int y = this->getY()*punto2.getY();
-    int z = this->getZ()*punto2.getZ();
-
-    Punto3D resultado (x, y, z);
-
-    return resultado;
-}
-
-Punto3D& Punto3D::operator* (int escalar)const
-{
-    int x = (this->getX()*escalar);
-    int y = (this->getY()*escalar);
-    int z = (this->getZ()*escalar);
-
-    Punto3D resultado (x, y, z);
-    
-    return resultado;
 }
