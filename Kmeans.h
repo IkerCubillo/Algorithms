42c6fb4bbf70c9992a068e6be7a5d1c58c0c1efe
IkerCubillo <i.cubillo@opendeusto.es>
2023-06-15 11:37:56 +0200

    Clase Kmeans

---
+++ Kmeans.h
@@ -0,0 +1,26 @@
+#ifndef KMEANS_H_
+#define KMEANS_H_
+
+#include "Cluster.h"
+
+class KMeans
+{
+	int numPuntos;
+    Punto* arrayPuntos;
+    int k;
+    Cluster* arrayClusters;
+
+public:
+	KMeans();
+	KMeans(int numPuntos, Punto* arrayPuntos, int k, Cluster* arrayClusters);
+	KMeans(const KMeans &km);
+	~KMeans();
+
+	int getNumPuntos() const;
+    Punto* getArrayPuntos() const;
+	int getK() const;
+    Cluster* getArrayClusters() const;
+
+};
+
+#endif /* KMEANS_H_ */
